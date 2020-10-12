0afef48df80d0cd79bdc7fcfc0da457f5bffaa5e
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2020-10-12 01:07:32 -0300

    Renamed presentation project root namespace

--- src/CsBlog.Presentation/Program.cs
+++ src/CsBlog.Presentation/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Presentation
+namespace CsBlog.Presentation
 {
     public class Program
     {
